cf18693e46d4618842a20271ef825b488688d9f1
Ion Villarreal <ivillarreal@agetic.gob.bo>
2024-05-08 20:24:25 -0400

    feat: ejemplos de componentes MUI y grillas

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Box } from '@mui/material'
+import GridExample from '../components/samples/GridExample'
+import FlexboxExample from '../components/samples/FlexboxExample'
+import FlexboxExamplePage from './FlexboxExamplePage'
+import MaterialUIExamples from '../components/samples/MuiExamples'
+
+function MainPage() {
+  return (
+    <Box>
+      {/*<GridExample />*/}
+      {/*<FlexboxExamplePage />*/}
+      <MaterialUIExamples />
+    </Box>
+  )
+}
+
+export default MainPage
